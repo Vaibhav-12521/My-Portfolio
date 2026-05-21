2b3eb52778e296a6db2e7e86a83979c6858d265e
Vaibhav Singh <singh12521vaibhav@gmail.com>
2026-05-21 12:10:42 +0530

    update 2.2

--- js/script.js
+++ js/script.js
@@ -32,7 +32,6 @@ let header = document.querySelector('.header');
 header.classList.toggle('sticky', window.scrollY > 100);
 
 
-/*========== remove menu icon navbar when click navbar link (scroll) ==========*/
 menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
 
